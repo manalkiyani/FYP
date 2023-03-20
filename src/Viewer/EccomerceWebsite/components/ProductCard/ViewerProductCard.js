01e6e791939e1d9310710bae16b6c5cf6a173a8c
Fahad Rehman <wolfboy0786@gmail.com>
2023-03-20 05:22:21 +0500

    rating done, review in progress

--- src/Viewer/EccomerceWebsite/components/ProductCard/ViewerProductCard.js
+++ src/Viewer/EccomerceWebsite/components/ProductCard/ViewerProductCard.js
@@ -5,16 +5,55 @@ import { useEffect } from "react";
 import { getListOfProducts } from ".././../../../utilityFunctions/axiosFunctions";
 import { Button } from "@mui/material";
 import axios from "axios";
+import Rating from '@mui/material/Rating';
+import { styled } from '@mui/material/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import Reviews from "./Reviews";
+
 
 const ViewerProductCard = ({ searchedquery, productIds, sortOrder, sortfunc }) => {
   const [products, setProducts] = useState(null);
-  const [open1, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const [currentProduct, setCurrentProduct] = useState(null);
+  const [ratingValue, setRatingValue] = useState(0);
+  const [productRatingChange, setProductRatingChange] = useState(false); // new state to track changes in product ratings
 
+
+
+
+  const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+      color: '#FFD700',
+    },
+  });
   const handleClickOpen = (product) => {
     setOpen(true); /// this will open Dialogue box at the bottom of this file.
-    setCurrentProduct(product);
-    console.log("this is current product: " + product.name);
+
+
+  };
+  const handleClose = (product) => {
+    setOpen(false); /// this will open Dialogue box at the bottom of this file.
+
+
+  };
+
+
+
+  const handleRatingChange = async(event, newValue, productId) => {
+    setRatingValue(newValue);
+    console.log(newValue+" this is new val");
+    try {
+      await axios.post('http://localhost:8800/api/products/addrating', { productId, ratingValue });
+      alert('Rating added successfully');
+      console.log("this is product id clicked "+productId)
+      setProductRatingChange(!productRatingChange); // toggle productRatingChange state to trigger useEffect hook
+
+    } catch (err) {
+      console.log('Error adding rating:', err);
+      console.log("this is product id clicked in error "+productId)
+
+    }
   };
 
   const deleted = async (id) => {
@@ -51,7 +90,7 @@ const ViewerProductCard = ({ searchedquery, productIds, sortOrder, sortfunc }) =
   };
   useEffect(() => {
     getProducts();
-  }, [sortOrder]);
+  }, [sortOrder, ratingValue, productRatingChange]);
 
   const AddToCart = async (id, image, name, price, description) => {
     try {
@@ -79,6 +118,8 @@ const ViewerProductCard = ({ searchedquery, productIds, sortOrder, sortfunc }) =
     <div className={styles.productsContainer}>
       {products &&
         products.map((product) => {
+          let productId = product._id;
+          console.log("this is productid in return "+ productId )
           if (
             product.name.toLowerCase().includes(searchedquery.toLowerCase())
           ) {
@@ -88,6 +129,7 @@ const ViewerProductCard = ({ searchedquery, productIds, sortOrder, sortfunc }) =
                 <img
                   className={styles.image}
                   src={product.image}
+
                   alt={"image"}
                 />
                
@@ -95,6 +137,7 @@ const ViewerProductCard = ({ searchedquery, productIds, sortOrder, sortfunc }) =
                   <div className={styles.flex}>
                     <h2 className={styles.name}>{product.name}</h2>
                     <p className={styles.price}>{product.price + "$"}</p>
+                    <p className={styles.avgrating}> <p style={{color:'black', marginRight:'10px', marginTop:'-4px'}}>{product.avgRating}</p> <FontAwesomeIcon key={''} icon={faStar} /> </p>
                   </div>
                   <p className={styles.description}>{product.description}</p>
                     <Button
@@ -111,13 +154,26 @@ const ViewerProductCard = ({ searchedquery, productIds, sortOrder, sortfunc }) =
                   >
                     Add to Cart
                   </Button>
-                </div>
+
+                  <StyledRating    onChange={(event, newValue) => handleRatingChange(event, newValue, productId)} 
+                  name="size-large" defaultValue={5} size="large" />
+
+                  </div>
+                  <Button
+                    onClick={handleClickOpen}
+                    className={styles.button}
+                  >
+                    Review
+                  </Button>
+                  {open && <Reviews productId={productId} open={open} setOpen={setOpen} handleClickOpen={handleClickOpen} handleClose={handleClose}></Reviews>}
+
               </div>
+              
+              
             );
           }
         })}
 
-     
     </div>
   );
 };
